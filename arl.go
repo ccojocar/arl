2d4be90ad69f5be308d3dd223d20f0dabc217c43
Cosmin Cojocar <cosmc@microsoft.com>
2017-04-11 16:55:44 +0200

    Add a tool to measure the rate-limt of REST API

---
+++ arl.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const probeIntervalSec = 30
+
+var (
+	resource         string
+	tenantID         string
+	clientID         string
+	numTokens        int
+	parallelRequests int
+)
+
+func init() {
+	flag.StringVar(&resource, "resource", "", "REST resource for which the rate limit test is executed")
+	flag.StringVar(&tenantID, "tenant-id", "", "tenant ID")
+	flag.StringVar(&clientID, "client-id", "", "client ID")
+	flag.IntVar(&numTokens, "num-tokens", 1, "number of tokens requested for a user")
+	flag.IntVar(&parallelRequests, "parallel-reqs", 8, "number of parallel request")
+
+	flag.Parse()
+
+	if numTokens < 1 {
+		log.Fatal("number of tokens requested for a use must be at least 1")
+	}
+}
+
+func fetchTokens(tokenSource TokenSource, num int) ([]string, error) {
+	token, err := tokenSource.Token()
+	if err != nil {
+		return nil, err
+	}
+
+	var tokens []string
+	tokens = append(tokens, token)
+
+	for i := 2; i <= num; i++ {
+		token, err := tokenSource.Refresh()
+		if err != nil {
+			return nil, err
+		}
+		tokens = append(tokens, token)
+	}
+
+	return tokens, nil
+}
+
+func get(URL string, token string) (int, error) {
+	client := &http.Client{
+		Timeout: time.Minute * 10,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return errors.New("redirect not allowed")
+		},
+	}
+
+	req, err := http.NewRequest(http.MethodGet, URL, nil)
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode, nil
+}
+
+type ratelimitProbe struct {
+	URL   string
+	token string
+}
+
+func measureRatelimit(URL string, token string, parallelRequests int, abort chan struct{}) {
+	ratelimitProbes := make(chan ratelimitProbe, parallelRequests)
+	ratelimitReached := make(chan struct{})
+	errorChan := make(chan error)
+
+	var numReqs uint64
+	var wg sync.WaitGroup
+	defer wg.Wait()
+
+	start := time.Now()
+	for i := 0; i < parallelRequests; i++ {
+		wg.Add(1)
+		go func() {
+			for probe := range ratelimitProbes {
+				httpStatus, err := get(probe.URL, probe.token)
+				if err != nil {
+					errorChan <- err
+				} else if httpStatus == http.StatusOK {
+					atomic.AddUint64(&numReqs, 1)
+				} else if httpStatus == http.StatusTooManyRequests {
+					close(ratelimitReached)
+				}
+				wg.Done()
+			}
+		}()
+	}
+
+	for {
+		select {
+		case <-ratelimitReached:
+			end := time.Now()
+			close(ratelimitProbes)
+			currentNumReqs := atomic.SwapUint64(&numReqs, 0)
+			ratelimitDuration := end.Sub(start)
+			log.Printf("Rate limit reached at: %4.2f request/sec\n", float64(currentNumReqs)/ratelimitDuration.Seconds())
+			return
+		case <-abort:
+			close(ratelimitProbes)
+			log.Println("Aborting before reaching the rate limit")
+			return
+		case probeErr := <-errorChan:
+			close(ratelimitProbes)
+			log.Printf("failed to execute the rate limit probe: %v", probeErr)
+			return
+		default:
+			ratelimitProbes <- ratelimitProbe{URL, token}
+		}
+	}
+}
+
+func main() {
+	resourceURL, err := url.ParseRequestURI(resource)
+	if err != nil {
+		log.Fatalf("failed to parse the resource URL: %v", err)
+	}
+
+	authority := fmt.Sprintf("%s//%s/", resourceURL.Scheme, resourceURL.Host)
+
+	azureTokenSource, err := NewAzureTokenSource(tenantID, clientID, authority)
+	if err != nil {
+		log.Fatalf("failed to create the token source: %v", err)
+	}
+
+	tokens, err := fetchTokens(azureTokenSource, numTokens)
+	if err != nil {
+		log.Fatalf("failed to acquire %d tokens: %v", numTokens, err)
+	}
+
+	// register the interrupt handler
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	abort := make(chan struct{})
+	var wg sync.WaitGroup
+	for _, token := range tokens {
+		wg.Add(1)
+		go func(URL string, token string) {
+			measureRatelimit(URL, token, parallelRequests, abort)
+			wg.Done()
+		}(resource, token)
+	}
+
+	// wait until the program is interrupted
+	<-interrupt
+
+	log.Println("Waiting for rate limit probes to complete...")
+
+	close(abort)
+
+	// wait for all requests to complete
+	wg.Wait()
+}
---
+++ auth.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"errors"
+	"github.com/cosmincojocar/adal"
+	"sync"
+)
+
+const authority = "https://login.microsoftonline.com/"
+
+//TokenSource interface which should be implemented by an access token provider
+type TokenSource interface {
+	Token() (string, error)
+	Refresh() (string, error)
+}
+
+// AzureTokenSource is the Azure access token provider
+type AzureTokenSource struct {
+	lock        sync.Mutex
+	oauthConfig adal.OAuthConfig
+	clientID    string
+	resource    string
+	spt         *adal.ServicePrincipalToken
+}
+
+// NewAzureTokenSource create a new Azure token source
+func NewAzureTokenSource(tenantID string, clientID string, resource string) (*AzureTokenSource, error) {
+	oauthConfig, err := adal.NewOAuthConfig(authority, tenantID)
+	if err != nil {
+		return nil, err
+	}
+	return &AzureTokenSource{
+		oauthConfig: *oauthConfig,
+		clientID:    clientID,
+		resource:    resource,
+		spt:         nil,
+	}, nil
+}
+
+// Token returns a new access token
+func (ts *AzureTokenSource) Token() (string, error) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+	var err error
+	ts.spt, err = ts.acquireTokenDeviceCodeFlow()
+	return ts.spt.AccessToken, err
+}
+
+// Refresh refreshes an existing and returns its new value
+func (ts *AzureTokenSource) Refresh() (string, error) {
+	ts.lock.Lock()
+	defer ts.lock.Unlock()
+	if ts.spt == nil {
+		return "", errors.New("service principal token is nil. call Token() before Refresh()")
+	}
+	err := ts.spt.Refresh()
+	return ts.spt.AccessToken, err
+}
+
+func (ts *AzureTokenSource) acquireTokenDeviceCodeFlow() (*adal.ServicePrincipalToken, error) {
+	callback := func(token adal.Token) error {
+		return nil
+	}
+	oauthClient := &http.Client{}
+	deviceCode, err := adal.InitiateDeviceAuth(
+		oauthClient,
+		ts.oauthConfig,
+		ts.clientID,
+		ts.resource)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to start device auth flow: %s", err)
+	}
+
+	fmt.Println(*deviceCode.Message)
+
+	token, err := adal.WaitForUserCompletion(oauthClient, deviceCode)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to finish device auth flow: %s", err)
+	}
+
+	spt, err := adal.NewServicePrincipalTokenFromManualToken(
+		ts.oauthConfig,
+		ts.clientID,
+		resource,
+		*token,
+		callback)
+	return spt, err
+}
